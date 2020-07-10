7313d1779cd7c64283b3d7e2a87251ec2e891231
arsakhanov <baki_12_95@mail.ru>
2020-07-10 10:16:28 +0300

    Готовый алгоритм AStar

--- src/ru/arsakhanov/Main.java
+++ src/ru/arsakhanov/Main.java
@@ -3,6 +3,14 @@ package ru.arsakhanov;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        AStar aStar = new AStar(5, 5, 0, 0, 3, 2,
+                new int[][]{
+                        {0, 4}, {2, 2}, {3, 1}, {3, 3}, {2, 1}, {2, 3}
+                }
+        );
+        aStar.display();
+        aStar.process(); //Apply A* algorithm
+        aStar.displayScores(); //Display Scores on grid
+        aStar.displaySolution(); // Display Solution Path
     }
 }
